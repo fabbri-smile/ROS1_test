9b60a5067421149f3e39f5ddb62a1927cf8a45f2
fabbri-smile <kawamoto.ryuji@gmail.com>
2024-01-08 10:51:26 +0900

    gazebo勉強中

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-roslaunch ros1_test pubsub.launch
+cd ~/catkin_ws/src/gazebo_lecture/models
+export GAZEBO_MODEL_PATH=`pwd`:$GAZEBO_MODEL_PATH
+cd ~/catkin_ws
+
+# roslaunch gazebo_ros empty_world.launch world_name:=world_first_launch.world
+roslaunch gazebo_ros empty_world.launch world_name:=box.world
+# roslaunch ros1_test pubsub.launch
